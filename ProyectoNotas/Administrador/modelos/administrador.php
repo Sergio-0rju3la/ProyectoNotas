229243ee69aecf514460d32ba1d524eb06663fd9
NelsitonContrerasD <129330952+NelsitonContrerasD@users.noreply.github.com>
2023-06-20 15:48:22 -0500

    ahora si

--- ProyectoNotas/Administrador/modelos/administrador.php
+++ ProyectoNotas/Administrador/modelos/administrador.php
@@ -24,22 +24,61 @@ class Administrador extends conexion{
             header(location:'../pages/agregar.php')
         }
     }
-    //funcion para consultar usuarios
-    public function getadmin(){
-
+//funcion para consultar usuarios
+public function getadmin(){
+    $row=null;
+    $statement=$this->db->prepare("SELECT* from Usuarios where Perfil=Administrador");
+    $statement->execute();
+    while($result->statement->fetch()){
+        $row[]=$result;
     }
-    //funcion para listar por id especifico
-    public function getidad($ID){
-
+    result $row;
+}
+//funcion para listar por id especifico
+public function getidad($ID){
+    $row=null;
+    $statement=$this->db->prepare("SELECT * from Usuarios where id_usuario=:ID and Perfil='Administrador'");
+    $statement->bindParam(':ID',$ID);
+    $statement->execute();
+    while($result->$statement->fetch()){
+        $row[]=$result
     }
+}
     //funcion actualizar los datos del usuario
-    public function updatead($id,$Nombre,$Apellido,$Usuario,$Passwor,$Estado)
+    public function updatead($ID,$Nombre,$Apellido,$Usuario,$Passwor,$Estado)
     {
-
+        $statement=$this->bd->prepare("UPDATE usuarios SET Nombre=:Nombre,
+        Apellido=:Apellido,
+        Usuario=:Usuario,
+        Passwor=:Passwor,
+        Estado=:Estado WHERE id_usuario=$ID");
+        $statement->bindParam(':ID',$ID);
+        $statement->bindParam(':Nombre',$Nombre);
+        $statement->bindParam(':Apellido',$Apellido);
+        $statement->bindParam(':Usuario',$Usuario);
+        $statement->bindParam(':Passwor',$Passwor);
+        $statement->bindParam(':Estado',$Estado);
+        if($statement->execute())
+        {
+            header('location: ../pages/index.php');
+        }else{
+            echo "el usuario no";
+            header('location: ../pages/editar.php');
+        }
     }
     //funcion eliminar usuario
     public function deletead($ID){
+       
+        $statement=$this->db->prepare("DELETE FROM usuarios WHERE id_usuarios=:ID"):
+        $statement->bindParam(':ID',$ID);
+        if($statement->execute()){
+            echo "usuario eliminado";
+            header('location: ..pages/index.php');
 
+        }else{
+            echo "error no se puede eliminar";
+            header ('location: ../pages/eliminar.php');
+        }
     }
 }
 ?>
