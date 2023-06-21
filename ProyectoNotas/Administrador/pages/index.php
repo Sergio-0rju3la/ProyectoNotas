c1b57bf61ebd4c7ce0619e197d36ea053aadfc8d
NelsitonContrerasD <129330952+NelsitonContrerasD@users.noreply.github.com>
2023-06-21 16:54:05 -0500

    pe

---
+++ ProyectoNotas/Administrador/pages/index.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina principal ||</title>
+    <link rel="stylesheet" href="../../css/iniadmin.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="d col-12 conteiner-fluid"> 
+        <div class="col-6">
+            <img src="../../img/libro-abierto.png" alt="">
+            <a href="" class="z">Usuarios</a>
+            <a href="../../Materias/pages/agregar.php" class="z">Materias</a>
+            <a href="../../Estudiantes/pages/agregar.php" class="z">Estudiantes</a>
+            <a href="../../Docentes/pages/agregar.php" class="z">Docentes</a>
+
+        </div>
+        <div class=" col-6">
+        <input type="button" value="cerrar sesión" class="s btn btn-outline-danger">
+        </div>
+
+    </nav>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</html>
