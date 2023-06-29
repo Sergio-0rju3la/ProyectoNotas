fe4bb10cabcc5b6a6ecb995016f0b1125f9659da
NelsitonContrerasD <129330952+NelsitonContrerasD@users.noreply.github.com>
2023-06-29 17:08:43 -0500

    yepa1

---
+++ ProyectoNotas/Estudiantes/pages/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina principal ||</title>
+    <link rel="stylesheet" href="../../css/iniEstud.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="d col-12 conteiner-fluid"> 
+        <div class="col-2">
+            <img src="../../img/libro-abierto.png" alt="">
+            
+
+        </div>
+        <div class="col-8">
+        <h1>Estudiantes<h1></div>
+        <div class=" col-2">
+        <input type="button" value="cerrar sesión" class="s btn btn-outline-danger">
+        </div>
+
+    </nav>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</html>
